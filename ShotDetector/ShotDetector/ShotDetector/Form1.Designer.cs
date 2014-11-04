de14f3a56caa4d47eceb15685eb9ba88a358b424
AndreasDL <andreasdelille@hotmail.com>
2014-11-04 14:05:20 +0100

    gui partially complete

--- ShotDetector/ShotDetector/ShotDetector/Form1.Designer.cs
+++ ShotDetector/ShotDetector/ShotDetector/Form1.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.btnBrowse = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtFileName = new System.Windows.Forms.TextBox();
             this.ofdBrowse = new System.Windows.Forms.OpenFileDialog();
             this.mnMenu = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -40,15 +40,29 @@
             this.playToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.pauseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.serviceController1 = new System.ServiceProcess.ServiceController();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.shotNumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.StartFrame = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tags = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbMethod = new System.Windows.Forms.ComboBox();
             this.mnMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnBrowse
             // 
             this.btnBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnBrowse.Location = new System.Drawing.Point(793, 34);
+            this.btnBrowse.Location = new System.Drawing.Point(395, 28);
+            this.btnBrowse.Margin = new System.Windows.Forms.Padding(2);
             this.btnBrowse.Name = "btnBrowse";
-            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.Size = new System.Drawing.Size(56, 27);
             this.btnBrowse.TabIndex = 0;
             this.btnBrowse.Text = "browse";
             this.btnBrowse.UseVisualStyleBackColor = true;
@@ -57,21 +71,23 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 37);
+            this.label1.Location = new System.Drawing.Point(9, 35);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(30, 16);
+            this.label1.Size = new System.Drawing.Size(27, 15);
             this.label1.TabIndex = 1;
             this.label1.Text = "File";
             // 
-            // textBox1
+            // txtFileName
             // 
-            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.txtFileName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(63, 34);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(724, 22);
-            this.textBox1.TabIndex = 2;
-            this.textBox1.Text = "c:\\csi.avi";
+            this.txtFileName.Location = new System.Drawing.Point(40, 32);
+            this.txtFileName.Margin = new System.Windows.Forms.Padding(2);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(351, 20);
+            this.txtFileName.TabIndex = 2;
+            this.txtFileName.Text = "c:\\csi.avi";
             // 
             // ofdBrowse
             // 
@@ -86,7 +102,8 @@
             this.playToolStripMenuItem});
             this.mnMenu.Location = new System.Drawing.Point(0, 0);
             this.mnMenu.Name = "mnMenu";
-            this.mnMenu.Size = new System.Drawing.Size(880, 24);
+            this.mnMenu.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.mnMenu.Size = new System.Drawing.Size(784, 28);
             this.mnMenu.TabIndex = 3;
             this.mnMenu.Text = "menuStrip1";
             // 
@@ -96,20 +113,20 @@
             this.openToolStripMenuItem,
             this.quitToolStripMenuItem});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
             this.fileToolStripMenuItem.Text = "File";
             // 
             // openToolStripMenuItem
             // 
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(114, 24);
             this.openToolStripMenuItem.Text = "Open";
             this.openToolStripMenuItem.Click += new System.EventHandler(this.browseFile);
             // 
             // quitToolStripMenuItem
             // 
             this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
-            this.quitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(114, 24);
             this.quitToolStripMenuItem.Text = "Quit";
             this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
             // 
@@ -120,41 +137,149 @@
             this.pauseToolStripMenuItem,
             this.stopToolStripMenuItem});
             this.playToolStripMenuItem.Name = "playToolStripMenuItem";
-            this.playToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.playToolStripMenuItem.Size = new System.Drawing.Size(48, 24);
             this.playToolStripMenuItem.Text = "Play";
             // 
             // playToolStripMenuItem1
             // 
             this.playToolStripMenuItem1.Name = "playToolStripMenuItem1";
-            this.playToolStripMenuItem1.Size = new System.Drawing.Size(105, 22);
+            this.playToolStripMenuItem1.Size = new System.Drawing.Size(116, 24);
             this.playToolStripMenuItem1.Text = "Play";
+            this.playToolStripMenuItem1.Click += new System.EventHandler(this.start_Click);
             // 
             // pauseToolStripMenuItem
             // 
             this.pauseToolStripMenuItem.Name = "pauseToolStripMenuItem";
-            this.pauseToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
+            this.pauseToolStripMenuItem.Size = new System.Drawing.Size(116, 24);
             this.pauseToolStripMenuItem.Text = "Pause";
+            this.pauseToolStripMenuItem.Click += new System.EventHandler(this.pause_Click);
             // 
             // stopToolStripMenuItem
             // 
             this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
-            this.stopToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
-            this.stopToolStripMenuItem.Text = "Stop";
+            this.stopToolStripMenuItem.Size = new System.Drawing.Size(116, 24);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Location = new System.Drawing.Point(12, 60);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(751, 325);
+            this.panel1.TabIndex = 4;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnStart.Location = new System.Drawing.Point(12, 391);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 5;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.start_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnPause.Location = new System.Drawing.Point(93, 391);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(75, 23);
+            this.btnPause.TabIndex = 7;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.pause_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.shotNumber,
+            this.StartFrame,
+            this.tags});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 436);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(760, 121);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // shotNumber
+            // 
+            this.shotNumber.HeaderText = "shotNumber";
+            this.shotNumber.Name = "shotNumber";
+            // 
+            // StartFrame
+            // 
+            this.StartFrame.HeaderText = "StartFrame";
+            this.StartFrame.Name = "StartFrame";
+            this.StartFrame.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // tags
+            // 
+            this.tags.HeaderText = "Tags";
+            this.tags.Name = "tags";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBar1.Location = new System.Drawing.Point(175, 392);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(588, 56);
+            this.trackBar1.TabIndex = 9;
+            this.trackBar1.TickFrequency = 0;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(456, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 15);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Method";
+            // 
+            // cmbMethod
+            // 
+            this.cmbMethod.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmbMethod.FormattingEnabled = true;
+            this.cmbMethod.Items.AddRange(new object[] {
+            "pixel",
+            "motion",
+            "global histogram",
+            "local histogram",
+            "generalized"});
+            this.cmbMethod.Location = new System.Drawing.Point(511, 32);
+            this.cmbMethod.Name = "cmbMethod";
+            this.cmbMethod.Size = new System.Drawing.Size(252, 21);
+            this.cmbMethod.TabIndex = 11;
             // 
             // ShotDetector
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(880, 553);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(784, 569);
+            this.Controls.Add(this.cmbMethod);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txtFileName);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnBrowse);
             this.Controls.Add(this.mnMenu);
             this.MainMenuStrip = this.mnMenu;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "ShotDetector";
             this.Text = "ShotDetector";
             this.mnMenu.ResumeLayout(false);
             this.mnMenu.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -164,7 +289,7 @@
 
         private System.Windows.Forms.Button btnBrowse;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtFileName;
         private System.Windows.Forms.OpenFileDialog ofdBrowse;
         private System.Windows.Forms.MenuStrip mnMenu;
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
@@ -174,6 +299,18 @@
         private System.Windows.Forms.ToolStripMenuItem playToolStripMenuItem1;
         private System.Windows.Forms.ToolStripMenuItem pauseToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem;
+        private System.ServiceProcess.ServiceController serviceController1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn shotNumber;
+        private System.Windows.Forms.DataGridViewTextBoxColumn StartFrame;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tags;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbMethod;
     }
 }
 
--- ShotDetector/ShotDetector/ShotDetector/Form1.cs
+++ ShotDetector/ShotDetector/ShotDetector/Form1.cs
@@ -12,11 +12,18 @@ using System.Runtime.InteropServices;
 namespace ShotDetector
 {
     public partial class ShotDetector : Form{
-
-        private DxPlay m_play;
+        enum State{
+            Uninit,
+            Stopped,
+            Paused,
+            Playing
+        }
+        State m_State = State.Uninit;
+        DxPlay m_play = null;
 
         public ShotDetector(){
             InitializeComponent();
+            cmbMethod.SelectedIndex = 0;
         }
 
         private void browseFile(object sender, EventArgs e){
@@ -29,9 +36,89 @@ namespace ShotDetector
                 MessageBox.Show("hi");
             }
         }
-
         private void quitToolStripMenuItem_Click(object sender, EventArgs e){
             Application.Exit();
+        }
+        private void start_Click(object sender, EventArgs e){
+            // If necessary, close the old file
+            if (m_State == State.Stopped){
+                // Did the filename change?
+                if (txtFileName.Text != m_play.FileName){
+                    // File name changed, close the old file
+                    m_play.Dispose();
+                    m_play = null;
+                    m_State = State.Uninit;
+                }
+            }
+
+            // If we have no file open
+            if (m_play == null){
+                try{
+                    // Open the file, provide a handle to play it in
+                    m_play = new DxPlay(panel1, txtFileName.Text);
+
+                    // Let us know when the file is finished playing
+                    m_play.StopPlay += new DxPlay.DxPlayEvent(m_play_StopPlay);
+                    m_State = State.Stopped;
+                }
+                catch (COMException ce){
+                    MessageBox.Show("Failed to open file: " + ce.Message, "Open Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+            // If we were stopped, start
+            if (m_State == State.Stopped){
+                btnStart.Text = "Stop";
+                m_play.Start();
+                btnPause.Enabled = true;
+                txtFileName.Enabled = false;
+                m_State = State.Playing;
+            }
+            // If we are playing or paused, stop
+            else if (m_State == State.Playing || m_State == State.Paused){
+                m_play.Stop();
+                btnPause.Enabled = false;
+                txtFileName.Enabled = true;
+                btnStart.Text = "Start";
+                btnPause.Text = "Pause";
+                m_State = State.Stopped;
+            }
+        }
+        private void pause_Click(object sender, System.EventArgs e)
+        {
+            // If we are playing, pause
+            if (m_State == State.Playing)
+            {
+                m_play.Pause();
+                btnPause.Text = "Resume";
+                m_State = State.Paused;
+            }
+            // If we are paused, start
+            else
+            {
+                m_play.Start();
+                btnPause.Text = "Pause";
+                m_State = State.Playing;
+            }
+        }
+
+        // Called when the video is finished playing
+        private void m_play_StopPlay(Object sender)
+        {
+            // This isn't the right way to do this, but heck, it's only a sample
+            CheckForIllegalCrossThreadCalls = false;
+
+            btnPause.Enabled = false;
+            txtFileName.Enabled = true;
+            btnStart.Text = "Start";
+            btnPause.Text = "Pause";
+
+            CheckForIllegalCrossThreadCalls = true;
+
+            m_State = State.Stopped;
+
+            // Rewind clip to beginning to allow DxPlay.Start to work again.
+            m_play.Rewind();
         }
     }
 }
