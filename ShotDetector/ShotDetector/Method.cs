f20c984770eb50b0d2859a7e7ab87915ef72d1db
AndreasDL <andreasdelille@hotmail.com>
2014-11-06 10:40:12 +0100

    map verplaatst

---
+++ ShotDetector/ShotDetector/Method.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Collections;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel;
+using System.IO;
+
+using DirectShowLib;
+
+public interface Method{
+
+    bool run();
+
+}
