96a223278c7fffc016ed4616e6ec1404dd4d2e4f
andlille <andreasdelille@hotmail.com>
2014-11-22 18:49:14 +0100

    Manual merge with Andrea

---
+++ ShotDetector/ShotDetector/Model/DetectionMethods/LocalHistogramMethod.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Collections;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel;
+using System.IO;
+
+using DirectShowLib;
+
+public class LocalHistogram: aShotDetectionMethod {
+    private List<int[]> previous_histograms;
+    private List<int[]> current_histograms;
+    
+    private int[] current_histogram;
+    private int[] previous_histogram;
+
+    private double threshold;
+    private int nrOfBins;
+    private int divider;
+    private int nrOfBlocks;
+
+    public LocalHistogram(double _threshold, int _nrOfBins, int _nrOfBlocks,ShotCollection shots): base(shots) {
+
+        this.threshold = _threshold;
+        this.nrOfBins = _nrOfBins;
+        this.divider = (int)(Math.Ceiling(255.0 / nrOfBins));
+        this.nrOfBlocks = _nrOfBlocks;
+        this.previous_histograms = new List<int[]>();
+        //this.current_histograms = new List<int[]>();
+    }
+
+    public override bool DetectShot(double SampleTime, IntPtr pBuffer, int BufferLen){
+        Debug.Assert(IntPtr.Size == 4, "Change all instances of IntPtr.ToInt32 to .ToInt64");
+        byte[] current = new byte[(videoHeight * videoWidth) * 3];
+        Marshal.Copy(pBuffer, current, 0, BufferLen);
+
+        int number = (int)Math.Sqrt((double)nrOfBlocks);
+        previous_histograms = current_histograms;
+        current_histograms = new List<int[]>();
+
+        int endCols = videoWidth - (videoWidth % number);
+        int endRows = videoHeight - (videoHeight % number);
+
+        int sizeCols = endCols / number;
+        int sizeRows = endRows / number;
+
+        // Create nrOfBlocks histograms of the current frame
+        for (int i = 0; i < endRows; i = i + sizeRows) {
+            for (int j = 0; j < endCols; j = j + sizeCols) {
+                
+                // hist is the histogram of the current block
+                int[] hist = new int[nrOfBins * 3];
+                for (int k = 0; k < hist.Length; k++) { hist[k] = 0; }//hist entries are initialized to 0
+
+                for (int x = i; x < sizeRows + i; x++) {
+                    for (int y = j; y < sizeCols + j; y++) {
+
+                        byte[] currPixel = getPixel(y, x, current);
+                        hist[(int)(currPixel[0] / divider)]++;
+                        hist[(int)(currPixel[1] / divider) + nrOfBins]++;
+                        hist[(int)(currPixel[2] / divider) + 2 * nrOfBins]++;
+
+                    }
+                }
+
+                current_histograms.Add(hist);
+            }
+        }
+
+        if (frameNumber != 0) {
+            double twoHistsDiff = 0;
+            for (int k = 0; k < current_histograms.Count; k++) {
+                for (int i = 0; i < nrOfBins * 3 - 1; i++) {
+                    twoHistsDiff += Math.Abs(current_histograms[k][i] - previous_histograms[k][i]);
+                }
+            }
+
+            return twoHistsDiff > threshold * BufferLen;
+        }else{
+            return true;
+        }
+    }
+
+    private byte[] getPixel(int x, int y, byte[] frame) {
+        int position = y * m_stride + 3 * x;
+
+        byte[] pixel = { frame[position], frame[position + 1], frame[position + 2] };
+
+        return pixel;
+    }
+
+}
